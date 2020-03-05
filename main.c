67c9996744c493b023e7d9413cf953173ca3db85
MONTEIRO-PEREIRA Rui-Miguel <Rui-Miguel.MONTEIRO-PEREIRA@cpnv.ch>
2020-03-05 16:53:41 +0100

    WIP

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+/*
+ * Projet: Bataille Navale
+ * Auteur: Rui Monteiro
+ * Date début du code: 05.03.2020
+ * Date fin du code:
+ */
 
-int main() {
-    printf("Hello, World!\n");
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    int grille;
+
+
     return 0;
 }
+
+//comandes importantes pour la suite
+
+
+
